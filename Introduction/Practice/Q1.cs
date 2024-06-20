df24a6e4d10222b0c3d5bd292e4c8c9ae71c7d4b
JhoanDev <jhoandev.log@gmail.com>
2024-06-19 22:53:26 -0300

    feat: exercicios de fixação basicos

---
+++ Introduction/Practice/Q1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Practice
+{
+    internal class Q2
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int horas = int.Parse(Console.ReadLine());
+            double valorHora = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("NUMBER = " + num);
+            Console.WriteLine("SALARY = " + (horas * valorHora).ToString("C"));
+
+        }
+    }
+}
---
+++ Introduction/Pratice2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Practice2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a, b, c;
+            string[] vet = Console.ReadLine().Split(' ');
+            a = double.Parse(vet[0]);
+            b = double.Parse(vet[1]);
+            c = double.Parse(vet[2]);
+            double pi = 3.14159;
+
+            Console.WriteLine("TRIANGULO = " + (a * c / 2).ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("CIRCULO = " + (pi * c * c).ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("TRAPEZIO = " + (((a + b) * c) / 2).ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("QUADRADO = " + (b * b).ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("RETANGULO = " + (a * b).ToString("F3", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
