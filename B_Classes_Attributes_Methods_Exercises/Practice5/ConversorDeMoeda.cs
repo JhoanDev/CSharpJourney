7ca4f420cbcbfbdd31b401fca789d47dedf22a86
JhoanDev <jhoandev.log@gmail.com>
2024-06-24 02:10:26 -0300

    feat: static

---
+++ B_Classes_Attributes_Methods_Exercises/Practice5/ConversorDeMoeda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice5
+{
+    internal class ConversorDeMoeda
+    {
+        public static double IOF = 0.06;
+
+        public static double ConverteParaReais(double cotacaoDoDolar, double qntDeDolares)
+        {
+            double valorEmReais = cotacaoDoDolar * qntDeDolares;
+            valorEmReais += valorEmReais * IOF;
+            return valorEmReais;
+        }
+    }
+}
---
+++ B_Classes_Attributes_Methods_Exercises/Practice5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Practice5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double cotacao, qntdolar;
+            Console.Write("Qual a cotacao do dolar: ");
+            cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Qual a quantidade que deseja comprar: ");
+            qntdolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Valor a ser pago em reais: " + ConversorDeMoeda.ConverteParaReais(cotacao,qntdolar).ToString("C"));
+        }
+    }
+}
