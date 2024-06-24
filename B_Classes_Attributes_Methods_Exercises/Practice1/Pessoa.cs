771df96c79edda68906de81731573e3086bc05bd
JhoanDev <jhoandev.log@gmail.com>
2024-06-24 01:06:06 -0300

    feat: Classes

---
+++ B_Classes_Attributes_Methods_Exercises/Practice1/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    internal class Pessoa
+    {
+        private string nome;
+        private int idade;
+
+        public Pessoa(string nome, int idade)
+        {
+            this.nome = nome;
+            this.idade = idade;
+        }
+
+        public string MaisVelha(Pessoa pessoa)
+        {
+            if (this.idade > pessoa.idade) return this.nome;
+            else if (this.idade < pessoa.idade) return pessoa.nome;
+            return "Sao da mesma idade";
+        }
+
+    }
+}
---
+++ B_Classes_Attributes_Methods_Exercises/Practice1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Practice1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa("Maria", 26);
+            Pessoa p2 = new Pessoa("Joao", 22);
+            Console.WriteLine("A pessoa com maior idade é: " + p1.MaisVelha(p2));
+        }
+    }
+}
