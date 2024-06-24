ba763a8a391f7f1b6a563ac068fae5252cbd2688
JhoanDev <jhoandev.log@gmail.com>
2024-06-24 01:16:04 -0300

    feat: class

---
+++ B_Classes_Attributes_Methods_Exercises/Practice2/Funcionario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice2
+{
+    internal class Funcionario
+    {
+        private string nome;
+        private double salario;
+
+        public Funcionario(string nome, double salario)
+        {
+            this.nome = nome;   
+            this.salario = salario;
+        }
+
+        public double MediaSalario(Funcionario funcionario)
+        {
+            return (this.salario + funcionario.salario) / 2.0;
+        }
+    }
+}
---
+++ B_Classes_Attributes_Methods_Exercises/Practice2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Practice2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1 = new Funcionario("Carlos Silva", 6300.00);
+            Funcionario f2 = new Funcionario("Maria Oliveira", 6700.00);
+            Console.WriteLine("A media salarial dos dois é: " + f1.MediaSalario(f2));
+        }
+    }
+}
