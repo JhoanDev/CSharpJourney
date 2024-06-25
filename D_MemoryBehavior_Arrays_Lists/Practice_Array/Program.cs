2ab51a6b8ace362f3f184ab0e6e067432b381889
JhoanDev <jhoandev.log@gmail.com>
2024-06-25 17:14:06 -0300

    feat: array

---
+++ D_MemoryBehavior_Arrays_Lists/Practice_Array/Program.cs
@@ -0,0 +1,45 @@
+﻿using Practice_Array;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Room[] vet = new Room[10];
+            int quantity = 0;
+            Console.Write("How many rooms will be rented? ");
+            quantity = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < quantity; i++)
+            {
+                Console.WriteLine("\nRent #" + (i+1));
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Number room: ");
+                int room = int.Parse(Console.ReadLine());
+
+                while (vet[room] != null)
+                {
+                    Console.WriteLine("Room is occupied. Unable to rent.");
+                    Console.Write("Please enter a new room number: ");
+                    room = int.Parse(Console.ReadLine());
+                }
+
+                vet[room] = new Room(name, email);
+            }
+
+            Console.WriteLine("\nBusy rooms");
+            for (int i = 0; i < vet.Length; i++)
+            {
+                if (vet[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vet[i]);
+                }
+            }
+        }
+    }
+}
---
+++ D_MemoryBehavior_Arrays_Lists/Practice_Array/Room.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Array
+{
+    internal class Room
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Room(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Email;
+        }
+    }
+}
