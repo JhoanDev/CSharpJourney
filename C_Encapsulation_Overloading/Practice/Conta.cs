eaf8d10a6824cd2d622fb2e881b684e6b153a9b6
JhoanDev <jhoandev.log@gmail.com>
2024-06-25 02:32:55 -0300

    feat: encapsulamento, gets,sets, proprieties, sobrecarga

---
+++ C_Encapsulation_Overloading/Practice/Conta.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class Conta
+    {
+        private string _nome;
+        public int Numero { get; private set; }
+        public double Saldo { get; private set; }
+
+        public Conta(string nome, int numero)
+        {
+            _nome = nome;
+            Numero = numero;
+            Saldo = 0.0;
+        }
+        public Conta(string nome, int numero, double saldo)
+        {
+            _nome = nome;
+            Numero = numero;
+            Saldo = saldo;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public void Deposito(double valor)
+        {
+            if (valor > 0)
+            {
+                Saldo += valor;
+            }
+            else
+            {
+                Console.WriteLine("Não foi possivel realizar o deposito.");
+            }
+        }
+
+        public void Saque(double valor)
+        {
+            if (valor > 0)
+            {
+
+                Saldo -= valor + 5; //Taxa de 5
+            }
+            else
+            {
+                Console.WriteLine("Não foi possivel realizar o saque.");
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + Numero + ", Titular: "
+                + _nome + ", Saldo: $ " + Saldo.ToString("F2");
+        }
+    }
+}
---
+++ C_Encapsulation_Overloading/Practice/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+
+namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int numero;
+            double deposito, saque;
+            char aux;
+            Conta p;
+
+            Console.Write("Insira o número da conta: ");
+            numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Insira o titular da conta: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            aux = char.Parse(Console.ReadLine());
+            if (aux == 's' || aux == 'S')
+            {
+                Console.Write("Insira o valor do depósito inicial: ");
+                deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                p = new Conta(nome, numero, deposito);
+            }
+            else
+            {
+                p = new Conta(nome, numero);
+            }
+
+
+            Console.WriteLine("\nDados da conta: ");
+            Console.WriteLine(p);
+
+            Console.Write("\nInsira um valor para depósito: ");
+            deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            p.Deposito(deposito);
+
+            Console.WriteLine("\nDados da conta atualizados: ");
+            Console.WriteLine(p);
+
+            Console.Write("\nInsira um valor para saque: ");
+            saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            p.Saque(saque);
+
+            Console.WriteLine("\nDados da conta atualizados: ");
+            Console.WriteLine(p);
+        }
+    }
+}
