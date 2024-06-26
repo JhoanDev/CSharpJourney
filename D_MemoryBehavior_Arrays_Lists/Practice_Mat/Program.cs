a520d1204bef5b332229494a457956fa9e30d685
JhoanDev <jhoandev.log@gmail.com>
2024-06-26 03:33:39 -0300

    feat: Matriz

---
+++ D_MemoryBehavior_Arrays_Lists/Practice_Mat/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Practice_Mat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert matrix order: ");
+            int order = int.Parse(Console.ReadLine());
+            int[,] mat = new int[order, order];
+            int i, j;
+            int negative = 0;
+            for (i = 0; i < order; i++)
+            {
+                for (j = 0; j < order; j++)
+                {
+                    Console.Write("Insert the number at position [" + i + "][" + j + "]: ");
+                    mat[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("\nMain diagonal: ");
+            for (i = 0; i < order; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+            for (i = 0; i < order; i++)
+            {
+                for (j = 0; j < order; j++)
+                {
+                    if (mat[i, j] < 0) negative++;
+                }
+            }
+            Console.WriteLine("\nNegative numbers =  " + negative);
+
+            Console.WriteLine("\n---Matrix---");
+            for (i = 0; i < order; i++)
+            {
+                for (j = 0; j < order; j++)
+                {
+                    Console.Write(mat[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ D_MemoryBehavior_Arrays_Lists/Practice_Mat2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Practice_Mat2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] entrada;
+            entrada = Console.ReadLine().Split(' ');
+            int row, column;
+            row = int.Parse(entrada[0]);
+            column = int.Parse(entrada[1]);
+
+            int[,] mat = new int[row, column];
+
+            for (int i = 0; i < row; i++)
+            {
+                entrada = Console.ReadLine().Split(' ');
+                for (int j = 0; j < column; j++)
+                {
+                    mat[i, j] = int.Parse(entrada[j]);
+                }
+            }
+
+            int target = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    if (mat[i, j] == target)
+                    {
+                        Console.WriteLine("\nPosition [" + i + ", " + j + "]");
+                        if (j - 1 >= 0) Console.WriteLine("Left: " + mat[i, j - 1]);
+                        if (j + 1 < column) Console.WriteLine("Right: " + mat[i, j + 1]);
+                        if (i - 1 >= 0) Console.WriteLine("Up: " + mat[i - 1, j]);
+                        if (i + 1 < row) Console.WriteLine("Donw: " + mat[i + 1, j]);
+                    }
+                }
+            }
+
+        }
+    }
+}
